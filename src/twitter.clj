ee3b722bd5a79b29f4f228c31c73ba5b9d9b9776
Matt Revelle <matt@mars.local>
2009-07-19 00:51:26 -0400

    Added kill switch for API limit exceeded and moved query to its own namespace.

--- src/twitter.clj
+++ src/twitter.clj
@@ -3,22 +3,51 @@
   (:use [clojure.http.client]
         [clojure.contrib.json.read])
   (:require [clojure.http.resourcefully :as resourcefully]
-            [clojure.contrib.str-utils :as str-utils]))
+            [clojure.contrib.str-utils :as str-utils]
+            [twitter.query :as query]))
 
-(def *twitter-url* "http://twitter.com")
+(def rate-limit-reached (atom nil))
+
+(def *twitter-uri* nil)
+
+(def *settings* {:base-uri "http://twitter.com"
+                 :search-uri "http://search.twitter.com"
+                 :basic-auth nil})
 
 (defn- twitter-request
-  "Appends the rest-uri to the twitter-url, makes the request and returns
+  "Appends the rest-uri to the twitter-uri, makes the request and returns
 the result of parsing the first part of the body.  It is assumed that JSON
 is being used."
   ([rest-uri]
-     (-> (request (url (str *twitter-url* rest-uri)))
-         :body-seq
-         first
-         read-json-string))
+     (if-not @rate-limit-reached
+       (binding [*twitter-uri* (or *twitter-uri* (*settings* :base-uri))]
+         (let [result (request (url (str *twitter-uri* rest-uri)) 
+                               :get 
+                               (when-let [auth (*settings* :basic-auth)]
+                                 {"Authorization" (str "Basic " auth)}))]
+           (condp #(if (coll? %1)  
+                     (first (filter (fn [x] (== x %2)) %1))
+                     (== %2 %1)) (:code result)
+             200 (-> result
+                     :body-seq
+                     first
+                     read-json-string)
+             304 nil
+             [400 401 403 404 406 500 502 503] (let [body (-> (first (:body-seq result))
+                                                              read-json-string)
+                                                     headers (:headers result)
+                                                     error-msg (body "error")
+                                                     request-uri (body "request")]
+                                                 (when (= (headers "X-RateLimit-Remaining") "0")
+                                                   (swap! rate-limit-reached (fn [_] true)))
+                                                 (throw (proxy [Exception] [(str error-msg ". [" request-uri "]")]
+                                                          (request [] (body "request"))
+                                                          (remaining-requests [] (headers "X-RateLimit-Remaining"))
+                                                          (rate-limit-reset [] (java.util.Date. 
+                                                                                (headers "X-RateLimit-Reset")))))))))))
   ([type rest-uri]
      (condp = type
-       :search (binding [*twitter-url* "http://search.twitter.com"]
+       :search (binding [*twitter-uri* (*settings* :search-uri)]
                  (twitter-request rest-uri))
        (twitter-request rest-uri))))
 
@@ -45,7 +74,22 @@ in the Search API."
 (defn followers 
   "IDs of a user's followers."
   [id]
-  (twitter-request (str "/followers/ids.json?user_id=" id)))
+  (try 
+   (twitter-request (str "/followers/ids.json?user_id=" id))
+   (catch Exception e
+     (if (== 0 (((proxy-mappings e) "remaining-requests") e))
+       (throw e)
+       nil))))
+
+(defn posts
+  ([]
+     (twitter-request (str "/statuses/public_timeline.json")))
+  ([user-id]
+     (twitter-request (str "/statuses/user_timeline.json?user_id=" user-id))))
+
+(defn rate-limit-info
+  []
+  (twitter-request "/account/rate_limit_status.json"))
 
 (defn search
   "Run a search query."
@@ -55,10 +99,3 @@ in the Search API."
      (search query rpp 1))
   ([query rpp page]
      (twitter-request :search (str "/search.json?q=" query "&rpp=" rpp "&page=" page))))
-
-(defn query-all
-  "Build up a search string where all terms are OR'd."
-  [& terms]
-  (url-encode 
-   (str-utils/str-join " OR " 
-                       (map #(str "\"" % "\"") terms))))
