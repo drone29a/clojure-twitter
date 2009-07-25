78947ec8e513428e7941a3aa5348778abad4e0bb
Matt Revelle <matt@mars.local>
2009-07-25 14:01:20 -0400

    Added rest of Twitter API.  Only update-profile-image and update-profile-background-image are unsupported.

--- src/twitter.clj
+++ src/twitter.clj
@@ -33,6 +33,8 @@
        ~@body)))
 
 (defmacro def-twitter-method
+  "Given basic specifications of a Twitter API method, build a function that will
+take any required and optional arguments and call the associated Twitter method."
   [method-name req-method req-url required-params optional-params handler]
   (let [required-fn-params (vec (sort (map #(symbol (name %))
                                            required-params)))
@@ -65,19 +67,45 @@
                                  {:use-expect-continue false})
                     :as :json))))))
 
-(def-twitter-method update-status
-  :post
-  "twitter.com/statuses/update.json"
-  [:status]
-  [:in-reply-to-status-id]
-  (comp #(:status (:content %)) status-handler))
+(def-twitter-method public-timeline
+  :get
+  "twitter.com/statuses/public_timeline.json"
+  []
+  []
+  (comp #(:content %) status-handler))
 
-(comment (defn update-status
-           [status in-reply-to-status-id]
-           (apply (comp #(:status (:content %)) status-handler)
-                  (http/post (str "http://" "twitter.com/statuses/update.json")
-                             :query {:status status
-                                     :in_reply_to_status_id in-reply-to-status-id}))))
+(def-twitter-method friends-timeline
+  :get
+  "twitter.com/statuses/friends_timeline.json"
+  []
+  [:since-id
+   :max-id
+   :count
+   :page]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method user-timeline
+  :get
+  "twitter.com/statuses/user_timeline.json"
+  []
+  [:id
+   :user-id
+   :screen-name
+   :since-id
+   :max-id
+   :count
+   :page]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method mentions
+  :get
+  "twitter.com/statuses/mentions.json"
+  []
+  [:since-id
+   :max-id
+   :count
+   :page]
+  (comp #(:content %) status-handler))
 
 (def-twitter-method show-status
   :get
@@ -85,6 +113,20 @@
   [:id]
   []
   (comp #(:content %) status-handler))
+
+(def-twitter-method update-status
+  :post
+  "twitter.com/statuses/update.json"
+  [:status]
+  [:in-reply-to-status-id]
+  (comp #(:status (:content %)) status-handler))
+
+(def-twitter-method destroy-status
+  :post
+  "twitter.com/statuses/destroy.json"
+  [:id]
+  []
+  (comp #(:status (:content %)) status-handler))
 
 (def-twitter-method show-user-by-id
   :get
@@ -97,6 +139,93 @@
   :get
   "twitter.com/users/show.json"
   [:screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method direct-messages
+  :get
+  "twitter.com/direct_messages.json"
+  []
+  [:since-id
+   :max-id
+   :count
+   :page]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method sent-direct-messages
+  :get
+  "twitter.com/direct_messages/sent.json"
+  []
+  [:since-id
+   :max-id
+   :count
+   :page]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method send-direct-message-to-id
+  :post
+  "twitter.com/direct_messages/new.json"
+  [:user-id
+   :text]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method send-direct-message-to-name
+  :post
+  "twitter.com/direct_messages/new.json"
+  [:screen-name
+   :text]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method destroy-direct-message
+  :post
+  "twitter.com/direct_messages/destroy.json"
+  [:id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method create-friendship-to-id
+  :post
+  "twitter.com/friendships/create.json"
+  [:user-id]
+  [:follow]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method create-friendship-to-name
+  :post
+  "twitter.com/friendships/create.json"
+  [:screen-name]
+  [:follow]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method destroy-friendship-to-id
+  :post
+  "twitter.com/friendships/destroy.json"
+  [:user-id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method destroy-friendship-to-name
+  :post
+  "twitter.com/friendships/destroy.json"
+  [:screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method show-friendship-by-ids
+  :get
+  "twitter.com/friendships/show.json"
+  [:source-id
+   :target-id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method show-friendship-by-names
+  :get
+  "twitter.com/friendships/show.json"
+  [:source-screen-name
+   :target-screen-name]
   []
   (comp #(:content %) status-handler))
 
@@ -128,24 +257,11 @@
   []
   (comp #(:content %) status-handler))
 
-(def-twitter-method public-timeline
+(def-twitter-method verifiy-credentials
   :get
-  "twitter.com/statuses/public_timeline.json"
+  "twitter.com/account/verify_credentials.json"
   []
   []
-  (comp #(:content %) status-handler))
-
-(def-twitter-method user-timeline
-  :get
-  "twitter.com/statuses/user_timeline.json"
-  []
-  [:id
-   :user-id
-   :screen-name
-   :since-id
-   :max-id
-   :count
-   :page]
   (comp #(:content %) status-handler))
 
 (def-twitter-method rate-limit-status
@@ -155,14 +271,219 @@
   []
   (comp #(:content %) status-handler))
 
-(comment (defn search
-           "Run a search query."
-           ([query]
-              (search query 100 1))
-           ([query rpp]
-              (search query rpp 1))
-           ([query rpp page]
-              (twitter-request :search (str "/search.json?q=" query "&rpp=" rpp "&page=" page)))))
+(def-twitter-method end-session
+  :post
+  "twitter.com/account/end_session.json"
+  []
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method update-delivery-device
+  :post
+  "twitter.com/account/update_delivery_device.json"
+  [:device]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method update-profile-colors
+  :post
+  "twitter.com/account/update_profile_colors.json"
+  []
+  [:profile-background-color
+   :profile-text-color
+   :profile-link-color
+   :profile-sidebar-fill-color
+   :profile-sidebar-border-color]
+  (comp #(:content %) status-handler))
+
+;; TODO: need to add file uplooad support
+(comment (def-twitter-method update-profile-image
+           :post
+           "twitter.com/account/update_profile_image.json"
+           [:image]
+           []
+           (comp #(:content %) status-handler))
+
+         (def-twitter-method update-profile-background-image
+           :post
+           "twitter.com/account/update_profile_background_image.json"
+           [:image]
+           [:title]
+           (comp #(:content %) status-handler)))
+
+(def-twitter-method update-profile
+  :post
+  "twitter.com/account/update_profile.json"
+  []
+  [:name 
+   :email
+   :url
+   :location
+   :description]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method favorites
+  :get
+  "twitter.com/favorites.json"
+  []
+  [:id
+   :page]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method create-favorite
+  :post
+  "twitter.com/favorites/create.json"
+  [:id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method destroy-favorite
+  :post
+  "twitter.com/favorites/destroy.json"
+  [:id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method notifications-follow-by-id
+  :post
+  "twitter.com/notifications/follow.json"
+  [:user-id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method notifications-follow-by-name
+  :post
+  "twitter.com/notifications/follow.json"
+  [:screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method notifications-leave-by-id
+  :post
+  "twitter.com/notifications/leave.json"
+  [:user-id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method notifications-leave-by-name
+  :post
+  "twitter.com/notifications/leave.json"
+  [:screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method create-block
+  :post
+  "twitter.com/blocks/create.json"
+  [:user-id-or-screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method destroy-block
+  :post
+  "twitter.com/blocks/destroy.json"
+  [:user-id-or-screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method block-exists-for-id
+  :get
+  "twitter.com/blocks/exists.json"
+  [:user-id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method block-exists-for-name
+  :get
+  "twitter.com/blocks/exists.json"
+  [:screen-name]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method blocking-users
+  :get
+  "twitter.com/blocks/blocking.json"
+  []
+  [:page]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method blocking-user-ids
+  :get
+  "twitter.com/blocks/blocking/ids.json"
+  []
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method saved-searches
+  :get
+  "twitter.com/saved_searches.json"
+  []
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method show-saved-search
+  :get
+  "twitter.com/saved_searches/show.json"
+  [:id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method create-saved-search
+  :post
+  "twitter.com/saved_searches/create.json"
+  [:query]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method destroy-saved-search
+  :post
+  "twitter.com/saved_searches/destroy.json"
+  [:id]
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method search
+  :get
+  "search.twitter.com/search.json"
+  [:q]
+  [:callback
+   :lang
+   :rpp
+   :page
+   :since-id
+   :geocode
+   :show-user]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method trends
+  :get
+  "search.twitter.com/trends.json"
+  []
+  []
+  (comp #(:content %) status-handler))
+
+(def-twitter-method current-trends
+  :get
+  "search.twitter.com/trends/current.json"
+  []
+  [:exclude]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method daily-trends
+  :get
+  "search.twitter.com/trends/daily.json"
+  []
+  [:date
+   :exclude]
+  (comp #(:content %) status-handler))
+
+(def-twitter-method weekly-trends
+  :get
+  "search.twitter.com/trends/weekly.json"
+  []
+  [:date
+   :exclude]
+  (comp #(:content %) status-handler))
 
 (defn status-handler
   [result]
