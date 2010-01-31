913476cb1e291da2dac9e15212365df477d1d4da
Jan Michael Alonzo <jmalonzo@webkit.org>
2010-01-31 21:10:17 +1100

    Leiningen build support

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-twitter "1.0.0-SNAPSHOT"
+  :description "Twitter Client API for Clojure"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+;[clj-oauth "1.0.0-SNAPSHOT"]
+                 [com.twinql.clojure/clj-apache-http "1.1.0"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.1.0"]]
+  )
