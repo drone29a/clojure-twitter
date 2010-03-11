fd426417bb05baaf1b2e754086de595555c5f0db
Matt Revelle <matt@dynamictyping.org>
2010-03-10 20:03:29 -0500

    Increased minor version, depend on newer clj-oauth.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject clojure-twitter "1.0.0-SNAPSHOT"
+(defproject clojure-twitter "1.1.0-SNAPSHOT"
   :description "Twitter Client API for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-;[clj-oauth "1.0.0-SNAPSHOT"]
-                 [com.twinql.clojure/clj-apache-http "1.1.0"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.1.0"]]
-  )
+                 [clj-oauth "1.1.0-SNAPSHOT"]
+                 [com.twinql.clojure/clj-apache-http "2.0.0"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]])
