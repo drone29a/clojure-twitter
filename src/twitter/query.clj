db2ed51b620cec7e3253e667a33ffe4f74d71980
Matt Revelle <matt@dynamictyping.org>
2010-07-29 20:18:25 -0400

    Updated to support new string api.

--- src/twitter/query.clj
+++ src/twitter/query.clj
@@ -1,11 +1,11 @@
 (ns twitter.query
   (:refer-clojure :exclude [or name])
-  (:require [clojure.contrib.str-utils :as str-utils]))
+  (:require [clojure.string :as string]))
 
 (defn or
   "Build up a search string where all terms are OR'd."
   [& terms]
   (java.net.URLEncoder/encode 
-   (str-utils/str-join " OR " 
-                       (map #(str "\"" % "\"") terms))
+   (string/join " OR " 
+                (map #(str "\"" % "\"") terms))
    "UTF-8"))
