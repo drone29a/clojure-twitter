035b4127e2fae475e46016627b57dda7bd875e86
Matt Revelle <matt@dynamictyping.org>
2010-07-29 20:17:03 -0400

    Updated clj-oauth, clj-apache-http, and swank-clojure versions.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Twitter Client API for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [clj-oauth "1.1.0-SNAPSHOT"]
-                 [com.twinql.clojure/clj-apache-http "2.0.0"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]])
+                 [clj-oauth "1.2.1"]
+                 [com.twinql.clojure/clj-apache-http "2.0.2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
