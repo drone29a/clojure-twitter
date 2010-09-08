363eb7b97e008723e24eaaf4dc1270219f9eaec4
Matt Revelle <matt@dynamictyping.org>
2010-09-07 23:37:54 -0400

    Updated version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-twitter "1.2.0"
   :description "Twitter Client API for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [clj-oauth "1.2.1"]
-                 [com.twinql.clojure/clj-apache-http "2.0.2"]]
+                 [com.twinql.clojure/clj-apache-http "2.3.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
