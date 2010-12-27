e83d650ad13506604db18690b014e30b2843ba74
Matt Revelle <mrevelle@gmail.com>
2010-12-26 21:19:37 -0500

    Updated version to depend on updated clj-oauth.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-twitter "1.2.3"
+(defproject clojure-twitter "1.2.4"
   :description "Twitter Client API for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-oauth "1.2.4"]
+                 [clj-oauth "1.2.9"]
                  [com.twinql.clojure/clj-apache-http "2.3.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
