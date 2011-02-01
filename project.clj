6d1b6df372b1d65c06c63c9d192e87897fb52b63
Matt Revelle <mrevelle@gmail.com>
2011-02-01 01:28:12 -0800

    Version bump for updated dep on clj-oauth.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-twitter "1.2.4"
+(defproject clojure-twitter "1.2.5"
   :description "Twitter Client API for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-oauth "1.2.9"]
+                 [clj-oauth "1.2.10"]
                  [com.twinql.clojure/clj-apache-http "2.3.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
