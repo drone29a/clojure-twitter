9c739e211545fefb9825cf42b0f56c53fc8eb62a
Matt Revelle <mrevelle@gmail.com>
2013-08-30 13:17:12 -0400

    Bumped version number for release.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojure-twitter "1.2.6-SNAPSHOT"
+(defproject clojure-twitter "1.2.8"
   :description "Twitter Client API for Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
-                 [clj-oauth "1.3.1-SNAPSHOT"]
+                 [clj-oauth "1.4.1"]
                  [org.clojars.tavisrudd/clj-apache-http "2.3.2-SNAPSHOT"]])
